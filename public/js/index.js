a7e6d99beae7a85e61d6d2764de31a541d02e790
Agnieszka Sierocinska <agn.sierocinska@gmail.com>
2018-11-12 21:00:25 +0100

    emit newMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,9 @@
 var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
-    socket.emit('createMessage', {
-        from: "Aga",
-        text: "Hi"
-    })
 });
 socket.on('newMessage', function (message){
-    console.log(`New message from ${message.from}: ${message.text}`)
+    console.log(`New message`, message)
 });
 socket.on('disconnect', function (){
     console.log('Disconnected from server')
